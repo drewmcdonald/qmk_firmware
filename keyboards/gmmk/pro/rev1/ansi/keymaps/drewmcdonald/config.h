df1785e1df6abb24eaaf3092dcb10f2abed6bbb7
Drew McDonald <drewmcdo@gmail.com>
2022-08-25 10:52:30 -0400

    bump debounce to 8

--- keyboards/gmmk/pro/rev1/ansi/keymaps/drewmcdonald/config.h
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/drewmcdonald/config.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#ifdef DEBOUNCE
+    #undef DEBOUNCE  // unset the default of 5
+    #define DEBOUNCE 8
+#endif
+
 #ifdef RGB_MATRIX_ENABLE
     #define RGB_DISABLE_TIMEOUT 600000     // 10 minutes (10 * 60 * 1000ms)
     #define RGB_DISABLE_WHEN_USB_SUSPENDED
